432fe41c7478fc84c9b29a069a5931e3d6408632
nall1994 <nunoleite31@gmail.com>
2018-12-18 00:55:34 +0000

    Update publicacao.js

--- myFacebook/models/publicacao.js
+++ myFacebook/models/publicacao.js
@@ -74,7 +74,10 @@ var publicacaoSchema = new Schema({
     data: {type:String, required:true},
     dados: PubSchema,
     classificacoes: [String],
-    comentarios : [String,String], // [usernameDoComentador, comentario],
+    comentarios : [{
+        username: {type: String, required:true},
+        comentario: {type: String, required:true}
+    }],
     groupId: {type: String},
     isPrivate: {type:Boolean, required:true, default:true}
 })
