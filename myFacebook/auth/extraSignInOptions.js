da1bf7830b919d835661aa5c422a653b434e3cdb
nall1994 <34685054+nall1994@users.noreply.github.com>
2019-01-28 22:54:02 +0000

    remoção de módulo desnecessário de autenticação

--- myFacebook/auth/extraSignInOptions.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    "app_id": "562097824291394",
-    "app_secret": "a774a94acb492ba7f725eeb76a96f96f",
-    "callbackUrl": "http://localhost:3000/users/login/facebook/callback"
-}
